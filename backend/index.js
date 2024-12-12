0ea4d7c3de6fa0bde3715f19f4eafb11f04c14ec
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-12-12 06:43:46 +0530

    connect backend to mongo atlas

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 8080
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const port = 8080;
+
+// Mongo connection
+const dbUrl = process.env.ATLAS_DB_URI;
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.error("Failed to connect:", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
